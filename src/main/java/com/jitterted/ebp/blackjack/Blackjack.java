c8987081956165b54e4f415759379d20fb108141
Ted M. Young <tedyoung@gmail.com>
2021-08-09 21:32:43 -0700

    Lab #3: Moved classes into DOMAIN and ADAPTER packages as appropriate.

--- src/main/java/com/jitterted/ebp/blackjack/Blackjack.java
+++ src/main/java/com/jitterted/ebp/blackjack/Blackjack.java
@@ -1,10 +1,15 @@
 package com.jitterted.ebp.blackjack;
 
+import com.jitterted.ebp.blackjack.adapter.in.console.ConsoleGame;
+import com.jitterted.ebp.blackjack.domain.Game;
+
+// Application Startup & Assembler
 public class Blackjack {
 
     public static void main(String[] args) {
         Game game = new Game();
-        ConsoleGame consoleGame = new ConsoleGame(game); // in general: Entities aren't directly passed in to Adapters
+        // REMINDER! In general, Entities aren't directly passed in to Adapters
+        ConsoleGame consoleGame = new ConsoleGame(game);
         consoleGame.start();
     }
 
