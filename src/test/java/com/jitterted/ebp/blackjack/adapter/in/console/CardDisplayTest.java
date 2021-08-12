c11f680ff5f8ebaa8965b50e64f13fa82339dbcf
Ted M. Young <tedyoung@gmail.com>
2021-08-11 20:55:42 -0700

    Lab #7: Fix dependency violation in CardDisplayTest.

--- src/test/java/com/jitterted/ebp/blackjack/adapter/in/console/CardDisplayTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/adapter/in/console/CardDisplayTest.java
@@ -6,8 +6,11 @@ import com.jitterted.ebp.blackjack.domain.Suit;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.fusesource.jansi.Ansi.ansi;
 
 public class CardDisplayTest {
+
+    private static final Rank DUMMY_RANK = Rank.TEN;
 
     @Test
     public void displayTenAsString() throws Exception {
@@ -25,5 +28,20 @@ public class CardDisplayTest {
                 .isEqualTo("[31m┌─────────┐[1B[11D│5        │[1B[11D│         │[1B[11D│    ♦    │[1B[11D│         │[1B[11D│        5│[1B[11D└─────────┘");
     }
 
+    @Test
+    public void suitOfHeartsOrDiamondsIsDisplayedInRed() throws Exception {
+        // given a card with Hearts or Diamonds
+        Card heartsCard = new Card(Suit.HEARTS, DUMMY_RANK);
+        Card diamondsCard = new Card(Suit.DIAMONDS, DUMMY_RANK);
+
+        // when we ask for its display representation
+        String ansiRedString = ansi().fgRed().toString();
+
+        // then we expect a red color ansi sequence
+        assertThat(ConsoleCard.display(heartsCard))
+                .contains(ansiRedString);
+        assertThat(ConsoleCard.display(diamondsCard))
+                .contains(ansiRedString);
+    }
 
 }
